53f6582d0a0c6157c4756af2c2915f8265f37b05
MohamedElmorsy <67465043+elmorsy6@users.noreply.github.com>
2023-06-03 21:55:30 +0300

    Add files via upload

---
+++ band name generator start.py
@@ -0,0 +1,7 @@
+# band-name-generator-start
+name = input("What's Your Name? ")
+print("Hello " + name)
+
+city = input("Which City did you grew up in ? ")
+pet = input("What is the name of a Pet ? ")
+print(city + " " + pet)
